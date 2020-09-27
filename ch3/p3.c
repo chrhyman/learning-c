af506b346de2d77c003adc7156c705c8fa3afaee
Chris Hyman <chris@wugs.me>
2020-09-26 17:49:53 -0700

    edit down question
    only include required info

--- ch3/p3.c
+++ ch3/p3.c
@@ -5,16 +5,7 @@
 // Chapter 3 of C Programming: A Modern Approach by K. N. King
 //
 // 3. Books are identified by an International Standard Book Number (ISBN).
-// ISBNs assigned after January 1, 2007 contain 13 digits, arranged in five
-// groups, such as 978-0-393-97950-3. (Older ISBNs use 10 digits.) The first
-// group (the `GS1 prefix`) is currently either 978 or 979. The `group
-// identifier` specifies the language or country of origin (for example, 0 and
-// 1 are used in English-speaking countries). The `publisher code` identifies
-// the publisher (393 is the code for W. W. Norton). The `item number` is
-// assigned by the publisher to identify a specific book (97950 is the code for
-// this book). An ISBN ends with a `check digit` that's used to verify the
-// accuracy of the preceding digits. Write a program that breaks down an ISBN
-// entered by the user:
+// Write a program that breaks down an ISBN entered by the user:
 //
 //      Enter ISBN: `978-0-393-97950-3`
 //      GS1 prefix: 978
---
+++ ch3/p4.c
@@ -0,0 +1,17 @@
+/*
+// Author: Chris Hyman, chris@wugs.me
+// Date: 26 Sept 2020
+// A solution to Programming Exercises and Projects from
+// Chapter 3 of C Programming: A Modern Approach by K. N. King
+//
+// 4.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  //
+
+  return 0;
+}
