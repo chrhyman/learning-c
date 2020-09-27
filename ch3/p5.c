5118d11f1e8200e096981751f76adb7c1ecdd898
Chris Hyman <chris@wugs.me>
2020-09-26 18:13:14 -0700

    Create p5.c, p6.c

---
+++ ch3/p5.c
@@ -0,0 +1,28 @@
+/*
+// Author: Chris Hyman, chris@wugs.me
+// Date: 26 Sept 2020
+// A solution to Programming Exercises and Projects from
+// Chapter 3 of C Programming: A Modern Approach by K. N. King
+//
+// 5. Write a program that asks the user to enter the numbers from 1 to 16 (in
+// any order) and then displays the numbers in a 4 by 4 arrangement, followed
+// by the sums of the rwos, columns, and diagonals:
+//
+//      Enter the numbers from 1 to 16 in any order:
+//      `16 3 2 13 5 10 11 8 9 6 7 12 4 15 14 1`
+//      16  3  2 13
+//       5 10 11  8
+//       9  6  7 12
+//       4 15 14  1
+//
+//      Row sums: 34 34 34 34
+//      Column sums: 34 34 34 34
+//      Diagonal sums: 34 34
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  return 0;
+}
---
+++ ch3/p6.c
@@ -0,0 +1,43 @@
+/*
+// Author: Chris Hyman, chris@wugs.me [includes copied code with notice]
+// Date: 26 Sept 2020
+// A solution to Programming Exercises and Projects from
+// Chapter 3 of C Programming: A Modern Approach by K. N. King
+//
+// 6. Modify the `addfrac.c` program of Section 3.2 so that the user enters
+// both fractions at the same time, separated by a plus sign:
+//
+//      Enter two fractions separated by a plus sign: `5/6+3/4`
+//      The sum is 38/24
+*/
+
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* addfrac.c (Chapter 3, page 46) */
+/* Adds two fractions */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int num1, denom1, num2, denom2, result_num, result_denom;
+
+  printf("Enter first fraction: ");
+  scanf("%d /%d", &num1, &denom1);
+
+  printf("Enter second fraction: ");
+  scanf("%d /%d", &num2, &denom2);
+
+  result_num = num1 * denom2 + num2 * denom1;
+  result_denom = denom1 * denom2;
+  printf("The sum is %d/%d\n", result_num, result_denom);
+
+  return 0;
+}
