d3a92277f71db452ad3ea2e39163be06f7099ce6
Chris Hyman <chris@wugs.me>
2020-09-26 18:00:54 -0700

    Update p4.c

--- ch3/p4.c
+++ ch3/p4.c
@@ -4,14 +4,22 @@
 // A solution to Programming Exercises and Projects from
 // Chapter 3 of C Programming: A Modern Approach by K. N. King
 //
-// 4.
+// 4. Write a program that prompts the user to enter a telephone number in the
+// form (xxx) xxx-xxxx and then displays the number in the form xxx.xxx.xxxx:
+//      Enter phone number [(xxx) xxx-xxxx]: `(404) 817-6900`
+//      You entered 404.817.6900
 */
 
 #include <stdio.h>
 
 int main()
 {
-  //
+  int area_code, prefix, suffix;
+
+  printf("Enter phone number [(xxx) xxx-xxxx]: ");
+  scanf(" (%d )%d -%d", &area_code, &prefix, &suffix);
+
+  printf("You entered %.3d.%.3d.%.4d\n", area_code, prefix, suffix);
 
   return 0;
 }
