62b095e5f433d5b10b090486893bcfe4bf04401c
Chris Hyman <chris@wugs.me>
2020-09-28 23:22:39 -0700

    Create ex05.c

---
+++ ch05/ex05.c
@@ -0,0 +1,15 @@
+/*
+// Author: Chris Hyman, chris@wugs.me
+// Date: 28 Sept 2020
+// A solution to Programming Exercises and Projects from
+// Chapter 5 of C Programming: A Modern Approach by K. N. King
+//
+// 5. Is the following `if` statement legal?
+//      if (n >= 1 <= 10)
+//          printf("...");
+// If so, what does it do when `n` is equal to 0?
+//
+//          Yes it is. No matter what value is passed for n, the if statement
+//          will execute. (_Bool) <= 10 is always true since it will only equal
+//          0 or 1.
+*/
