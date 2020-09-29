c59cbf58d84fc7f23192a64fd75d1a04d4544a0c
Chris Hyman <chris@wugs.me>
2020-09-28 23:23:49 -0700

    Create ex06.c

---
+++ ch05/ex06.c
@@ -0,0 +1,13 @@
+/*
+// Author: Chris Hyman, chris@wugs.me
+// Date: 28 Sept 2020
+// A solution to Programming Exercises and Projects from
+// Chapter 5 of C Programming: A Modern Approach by K. N. King
+//
+// 6. Is the following `if` statement legal?
+//      if (n == 1-10)
+//          printf("...");
+// If so, what does it do when `n` is equal to 5?
+//
+//          Yes it is. Nothing will occur because n is not equal to -9.
+*/
