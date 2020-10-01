cd4b3cb14473b35fa3ab307d0ad0238227268910
Chris Hyman <chris@wugs.me>
2020-09-30 21:29:58 -0700

    Create ex11.c

---
+++ ch05/ex11.c
@@ -0,0 +1,46 @@
+/*
+// Author: Chris Hyman, chris@wugs.me
+// Date: 28 Sept 2020
+// A solution to Programming Exercises and Projects from
+// Chapter 5 of C Programming: A Modern Approach by K. N. King
+//
+// 11. Write a `switch` statement that prints the corresponding city name if
+// present in the table in the textbook; otherwise display an error.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int area_code;
+
+    printf("Enter an area code: ");
+    scanf("%d", &area_code);
+
+    switch (area_code) {
+        case 229:
+            printf("Albany");
+            break;
+        case 404:
+        case 470:
+        case 678:
+        case 770:
+            printf("Atlanta");
+            break;
+        case 478:
+            printf("Macon");
+            break;
+        case 706:
+        case 762:
+            printf("Columbus");
+            break;
+        case 912:
+            printf("Savannah");
+            break;
+        default:
+            printf("Area code not recognized");
+            break;
+    }
+
+    return 0;
+}
