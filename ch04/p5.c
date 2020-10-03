55884d13ee584603ede21e2109e7fb2a26947b68
Chris Hyman <chris@wugs.me>
2020-10-03 01:08:27 -0700

    spacing

--- ch04/p5.c
+++ ch04/p5.c
@@ -24,18 +24,18 @@
 
 int main(void)
 {
-  int a, b, c, d, e, f, g, h, i, j, k,
-      first_sum, second_sum, total;
+    int a, b, c, d, e, f, g, h, i, j, k,
+        first_sum, second_sum, total;
 
-  printf("Enter the first 11 digits of a UPC: ");
-  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
-    &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k);
+    printf("Enter the first 11 digits of a UPC: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+        &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k);
 
-  first_sum = a + c + e + g + i + k;
-  second_sum = b + d + f + h + j;
-  total = 3 * first_sum + second_sum;
+    first_sum = a + c + e + g + i + k;
+    second_sum = b + d + f + h + j;
+    total = 3 * first_sum + second_sum;
 
-  printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
 
-  return 0;
+    return 0;
 }
--- ch04/p6.c
+++ ch04/p6.c
@@ -23,18 +23,18 @@
 
 int main(void)
 {
-  int a, b, c, d, e, f, g, h, i, j, k, l,
-      first_sum, second_sum, total;
+    int a, b, c, d, e, f, g, h, i, j, k, l,
+        first_sum, second_sum, total;
 
-  printf("Enter the first 12 digits of an EAN: ");
-  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
-    &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l);
+    printf("Enter the first 12 digits of an EAN: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+        &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l);
 
-  first_sum = b + d + f + h + j + l;
-  second_sum = a + c + e + g + i + k;
-  total = 3 * first_sum + second_sum;
+    first_sum = b + d + f + h + j + l;
+    second_sum = a + c + e + g + i + k;
+    total = 3 * first_sum + second_sum;
 
-  printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
 
-  return 0;
+    return 0;
 }
